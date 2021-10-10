035553df9bcf9121c910e0e08809ae75b46a042b
Thuylinh2802 <79526602+Thuylinh2802@users.noreply.github.com>
2021-10-10 17:45:18 +0700

    fix đồ họa

--- src/display/graphic/favoriteword/FavoriteController.java
+++ src/display/graphic/favoriteword/FavoriteController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import utility.ProjectConfig;
 
 import java.io.IOException;
